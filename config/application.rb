6ff7be05fbde7347fbdbc680889559d3ae1a80cb
PavelJ <pavel.jira@gmail.com>
2013-11-01 22:38:28 +0100

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module LearnRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #Potrebne pro fungovani bootstrapu od verze rails 4.0
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
