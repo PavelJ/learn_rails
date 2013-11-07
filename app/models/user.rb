05d6d240fefaebb2fa3157a946e7c9dd755fccf4
PavelJ <pavel.jira@gmail.com>
2013-11-07 20:51:11 +0100

    Make a basic user model including secure password

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+	# Nektere DB adaptery maji indexy case insensitive, coz by vadilo pri validoani unikatnosti
+	before_save { self.email = email.downcase }
+
+  	validates :name,  presence: true, length: { maximum: 50 }
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+    # Magie - vytvari virtualni sloupce password a password_confirmation - ty jsou
+    # jen v pameti a do DB se neukladaji, pridava take validace na presence a dalsi veci
+    has_secure_password
+
+    validates :password, length: { minimum: 6 }
+end
