5bfd7938f10325abeeeb19a00e131e9d415d7bda
PavelJ <pavel.jira@gmail.com>
2013-11-17 18:08:56 +0100

    Finish sign in

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,39 @@
+module SessionsHelper
+	def sign_in(user)
+		remember_token = User.new_remember_token
+		# Permanent je ve skutecnosti 20 let, v podstate se provadi nasledujici kod:
+		# cookies[:remember_token] = { value:   remember_token,
+        #                              expires: 20.years.from_now.utc }
+		cookies.permanent[:remember_token] = remember_token
+		# Tento update nevyvolava zadnou validaci user modelu, coz chceme, protoze nemame heslo atp.
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+		self.current_user = user
+	end
+
+	def signed_in?
+    	!current_user.nil?
+  	end
+
+	def current_user=(user)
+	    @current_user = user
+	end
+
+	# Nelze jen jednoduse vratit @current_user, vse je tu stateless, takze po kazdem requestu je @current_user nil
+	# a my si ho musime zase najit
+	def current_user
+	    remember_token = User.encrypt(cookies[:remember_token])
+	    # Pokud bychom ke current_user pristpovali vicekrat v ramci jednoho requestu, tak se nacte jen poprve, pak
+	    # uz si ho pamatujeme
+	    @current_user ||= User.find_by(remember_token: remember_token)
+	end
+
+	def destroy
+	    sign_out
+	    redirect_to root_url
+	end
+
+	def sign_out
+    	self.current_user = nil
+    	cookies.delete(:remember_token)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,30 @@
 class User < ActiveRecord::Base
 	# Nektere DB adaptery maji indexy case insensitive, coz by vadilo pri validoani unikatnosti
 	before_save { email.downcase! }
+  before_create :create_remember_token
 
-  	validates :name,  presence: true, length: { maximum: 50 }
-  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+	validates :name,  presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
 
-    # Magie - vytvari virtualni sloupce password a password_confirmation - ty jsou
-    # jen v pameti a do DB se neukladaji, pridava take validace na presence a dalsi veci
-    has_secure_password
+  # Magie - vytvari virtualni sloupce password a password_confirmation - ty jsou
+  # jen v pameti a do DB se neukladaji, pridava take validace na presence a dalsi veci
+  has_secure_password
 
-    validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
